5b4c41f1ff1b87b753e44b55b6e07adb84c7fcf6
lishanghan <lishanghan@126.com>
2019-08-15 18:05:57 +0800

    添加接口自动化Demo

--- src/main/java/com/lsh/demo/Demo.java
+++ src/main/java/com/lsh/demo/Demo.java
@@ -1,10 +1,13 @@
 package com.lsh.demo;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.lsh.utils.HttpClientUtils;
 import com.lsh.utils.LogConfiguration;
 import lombok.extern.log4j.Log4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +16,14 @@ public class Demo {
 
     public static void main(String args[]){
         LogConfiguration.initLog();
+
+        interfaceDemo();
+
+
+    }
+
+
+    public static void webUiDemo(){
 
         //设置浏览器驱动路径
         System.setProperty("webdriver.chrome.driver","src/main/resources/dirver/chrome/win/chromedriver.exe");
@@ -24,6 +35,8 @@ public class Demo {
         //设置等待页面加载时间
         driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
         //搜索框输入关键字
+        /*WebElement element = driver.findElement(By.id("kw"));
+        element.sendKeys("自动化测试教程");*/
         driver.findElement(By.id("kw")).sendKeys("自动化测试教程");
         //点击“百度一下”按钮进行搜索
         driver.findElement(By.id("su")).click();
@@ -33,7 +46,7 @@ public class Demo {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //获得实际结果 //*[@id="1"]/h3/a/em
+        //获得实际结果
         String actual = driver.findElement(By.xpath("//*[@id=\"1\"]/h3/a/em")).getText().trim();
         //判断实际结果与预期结果是否相等
         if(actual.equals("测试教程")){
@@ -44,18 +57,35 @@ public class Demo {
         //关闭
         driver.quit();
 
+    }
 
-        /*ITestContext context=null;
-        SeleniumUtils seleniumUtils = new SeleniumUtils();
-        seleniumUtils.openBrowser("chrome",context,"http://www.baidu.com",20);
-        seleniumUtils.waitForPageLoading(20);
-        seleniumUtils.findElelementBy(By.id("kw")).sendKeys("selenium3教程");
-        seleniumUtils.findElelementBy(By.id("su")).click();
-        //seleniumUtils.quit();*/
+    public static void interfaceDemo(){
 
+        String url = "http://192.168.52.26:8011/main/customer/unencryptdetail";
+        String param = "{\n" +
+                "    \"user\":{\n" +
+                "        \"userid\":\"ae5b49ab-3604-e911-814b-00505696cb23\",\n" +
+                "        \"brokerCode\":\"18836\",\n" +
+                "        \"username\":\"Liujie\",\n" +
+                "        \"fullName\":\"刘杰\",\n" +
+                "        \"deviceId\":\"1123123123132132\",\n" +
+                "        \"images\":\"\"\n" +
+                "    },\n" +
+                "    \"dataId\":\"2faa47f3-b1b7-4224-93ef-f6484af89c73\",\n" +
+                "    \"pageSize\":10,\n" +
+                "    \"brokerCode\":\"18836\",\n" +
+                "    \"userId\":\"ae5b49ab-3604-e911-814b-00505696cb23\",\n" +
+                "    \"page\":1\n" +
+                "}";
 
-
-
+        String response = HttpClientUtils.doHttpJsonPost(url,param);
+        JSONObject json = JSONObject.parseObject(response);
+        Integer code = json.getInteger("code");
+        if(code == 0){
+            log.info("接口测试通过");
+        }else{
+            log.error("接口测试失败");
+        }
 
 
 
---
+++ src/main/java/com/lsh/utils/HttpClientUtils.java
@@ -0,0 +1,155 @@
+
+package com.lsh.utils;
+
+import java.io.IOException;
+
+import lombok.extern.log4j.Log4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+
+
+
+/**
+ * @author lish
+ * @description:HttpClient 常用方法封装
+ * */
+@Log4j
+public class HttpClientUtils {
+	
+
+	/**HttpClient对象*/
+	public static CloseableHttpClient httpClient;
+	/**HttpResponse对象*/
+	public static CloseableHttpResponse response;
+	/**http 响应状态码*/
+	public static String httpResponseCode;
+	/**http 响应实体*/
+	public static HttpEntity httpResponseEntity;
+
+	
+	
+	/**
+	 * 封装post请求方法,请求与响应都是Json格式字符串
+	 * @param  requestURL 接口地址
+	 * @param  requestParameters 接口请求参数
+	 * @return String responseBody 返回请求响应
+	 * */
+	public static String doHttpJsonPost(String requestURL,String requestParameters){
+		
+		httpClient=HttpClients.createDefault();
+		
+		String responseBody=null;
+		//创建HttpPost对象
+		HttpPost httpPost=new HttpPost(requestURL);
+		
+		
+		
+		try {
+			//给httpPost设置JSON格式的参数
+			StringEntity stringEntity=new StringEntity(requestParameters,"utf-8");
+			//设置请求头
+			httpPost.setHeader("Content-type", "application/json");
+			httpPost.setHeader("Accept", "application/json");
+			httpPost.setEntity(stringEntity); 
+			
+			String beforeTime=DateFormat.getCurrentTime("yyyy-MM-dd HH:mm:ss:SSS");
+			log.info("***********************************************开始调用接口！******************************************************");
+			log.info("接口请求为："+requestParameters);
+			response=httpClient.execute(httpPost);
+			String afterTime=DateFormat.getCurrentTime("yyyy-MM-dd HH:mm:ss:SSS");
+			log.info("***********************************************接口调用结束！******************************************************");
+			log.info("接口调用耗时："+DateFormat.timeDifference(beforeTime, afterTime));
+			httpResponseCode=response.getStatusLine().getStatusCode()+"";
+			if(httpResponseCode.equals("200")){
+				log.info("发送Http请求成功！Http响应状态为：【"+httpResponseCode+"】");
+				
+				httpResponseEntity=response.getEntity();
+				responseBody=EntityUtils.toString(httpResponseEntity,"UTF-8");
+				
+				log.info("http请求响应为："+response);
+				log.info("接口响应为："+responseBody);
+				
+			}else{
+				log.error("发送Http请求失败！http响应状态码为【"+httpResponseCode+"】");
+			}
+			
+			
+		} catch (ClientProtocolException e) {
+			
+			e.printStackTrace();
+			log.error("ClientProtocolException",e);
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			log.error("IOException",e);
+		}
+		
+		return responseBody;
+		
+	}
+	
+	
+	
+	/**
+	 * https-post-json请求方法封装
+	 * @param 
+	 * */
+	public static void doHttpsPostJson(){
+		
+		
+	}
+	
+	/**关闭链接，释放资源 */
+	public static void close(){		
+		
+			try {
+				//关闭连接，释放资源
+				response.close();
+				httpClient.close();
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				log.error("IOException",e);
+			}
+			
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
